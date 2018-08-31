4f5a9eb03568db58ddae066fd240fb712d7737f4
damicavallo <damian_cavallo@hotmail.com>
2018-08-31 14:14:52 -0300

    update delete casi terminando

--- src/main/java/com/accenture/microempleado/repositories/EmpleadoRepository.java
+++ src/main/java/com/accenture/microempleado/repositories/EmpleadoRepository.java
@@ -1,17 +1,13 @@
 package com.accenture.microempleado.repositories;
 
 import com.accenture.microempleado.model.Empleado;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-<<<<<<< Updated upstream
-public interface EmpleadoRepository extends CrudRepository<Empleado,Long> {
-
-=======
 public interface EmpleadoRepository extends JpaRepository<Empleado,Long> {
-
     Optional<Empleado> findById(Long id);
     Boolean removeById(Long id);
->>>>>>> Stashed changes
 }
